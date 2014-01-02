5b418119804b1f44284c45cbaf07e03d8f72f4d2
Suhas <suhas@ubuntu.(none)>
2014-01-02 16:12:08 +0530

    Added topcoder template

---
+++ template_new_contest/template.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+#include<vector>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+//Func here
+
+
+//Main
+int main()
+{
+
+	
+	return 0;
+}
